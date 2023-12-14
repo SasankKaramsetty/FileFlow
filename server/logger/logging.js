466a6b7440dfbe123fdb3db708a6bbc5d5193e70
SasankKaramsetty <95477580+SasankKaramsetty@users.noreply.github.com>
2023-12-14 10:26:21 +0530

    Update logging.js

--- server/logger/logging.js
+++ server/logger/logging.js
@@ -1,22 +1,44 @@
-// const winston = require("winston");
+// // const winston = require("winston");
+// import winston from "winston";
+// const { combine, timestamp, json,printf } = winston.format;
+
+// const myFormat = printf(({ level, message, timestamp }) => {
+//     return `[${timestamp}] [${level}] [${message}]`;
+//   });
+
+
+// const logger = winston.createLogger({
+//   level: process.env.LOG_LEVEL || 'info',
+//   format: combine(
+//     winston.format.colorize(),
+//     timestamp({format: "HH:mm:ss"}),
+//     myFormat
+//   ),
+//   transports: [
+//     new winston.transports.Console(),
+//     new winston.transports.File({filename: 'logs/server.log'})
+//   ],
+// });
+
+// export default logger;
 import winston from "winston";
-const { combine, timestamp, json,printf } = winston.format;
-
-const myFormat = printf(({ level, message, timestamp }) => {
-    return `[${timestamp}] [${level}] [${message}]`;
-  });
-
+const { format } = winston;
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
-  format: combine(
-    winston.format.colorize(),
-    timestamp({format: "HH:mm:ss"}),
-    myFormat
+  format: format.combine(
+    format.timestamp({ format: 'yyyy-MM-dd HH:mm:ss.SSS' }), // Modified timestamp format
+    format.printf(info => `${info.timestamp} [%t] ${info.level.toUpperCase()} ${info.message}`)
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({filename: 'logs/server.log'})
+    new winston.transports.File({
+      filename: 'logs/server.log',
+      format: format.combine(
+        format.timestamp(),
+        format.json()
+      )
+    })
   ],
 });
 
